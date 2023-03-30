918f598c5d1610737932547a6d1281b84cbcb124
haozhihan <haozhi.han@stu.pku.edu.cn>
2023-03-30 23:06:12 +0800

    USE_C99

--- MyMath/mysqrt.cpp
+++ MyMath/mysqrt.cpp
@@ -1,6 +1,8 @@
 #include "include/mysqrt.h"
 
+#ifdef USE_C99
 #include <boost/math/special_functions.hpp>
+#endif
 #include <cmath>
 #include <iostream>
 
@@ -8,6 +10,9 @@
 double mysqrt(double num)
 {
     num = sqrt(num);
+#ifdef USE_C99
     std::cout << "use boost math round function: " << std::endl;
-    return boost::math::round<double>(num);
+    num = boost::math::round<double>(num);
+#endif
+    return num;
 }
