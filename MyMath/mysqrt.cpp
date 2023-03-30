78582cbf882e4111b6a80acd05b1c4aecf0db7e7
haozhihan <haozhi.han@stu.pku.edu.cn>
2023-03-30 22:50:24 +0800

    format

--- MyMath/mysqrt.cpp
+++ MyMath/mysqrt.cpp
@@ -1,11 +1,12 @@
-#include <iostream>
-#include <cmath>
-#include <boost/math/special_functions.hpp>
-
 #include "include/mysqrt.h"
 
+#include <boost/math/special_functions.hpp>
+#include <cmath>
+#include <iostream>
+
 // a hack square root calculation using simple operations
-double mysqrt(double num) {
+double mysqrt(double num)
+{
     num = sqrt(num);
     std::cout << "use boost math: " << std::endl;
     return boost::math::round<double>(num);
